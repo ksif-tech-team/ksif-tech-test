08f4b8ad2bd85068425d02fbb54e0fca3e1e23aa
Jiwoopark <shapley_aldebaran@kaist.ac.kr>
2018-12-20 14:08:45 +0900

    Update 20183244_jiwoopark.py

--- 20183244_jiwoopark.py
+++ 20183244_jiwoopark.py
@@ -1,5 +1,16 @@
 # Jiwoo Park Ksif Tech
+import pandas as pd
+import numpy as np
+
+price = pd.read_csv("./price.csv", index_col=[0], parse_dates=[0], names=["DATE", "PRICE"])
+price = price.dropna(0)
 
 def max_draw_down(series):
-    # calculate max_draw_down
-    pass
+    # calculate max draw down 
+    # find min Idx first if many select latest one
+    argmin = np.argmin(series)
+    peak = max(series[:argmin + 1])
+    trough = min(series)
+    return (trough - peak) / peak
+
+print("Sol : {}".format(max_draw_down(price.values)))
