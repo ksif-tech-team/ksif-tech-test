7113eeba184a70c2fa06f8faa70b6d298db5b444
chanstar9 <traintion9@gmail.com>
2018-12-31 14:48:55 +0900

    수정했습니다!

--- 20194139_최찬규.py
+++ 20194139_최찬규.py
@@ -1,22 +1,15 @@
-import numpy as np
-import pandas as pd
-
-## 파일 불러오기
+#파일 불러오기
 from pandas import read_csv
-df=read_csv("C:/Users/peter/Documents/ksif-tech-test/Price.csv")
+aapl=read_csv("C:/Users/peter/Documents/ksif-tech-test/Price.csv")
 
-## NaN 값 없애기
-df=df.dropna()
+#일별 수익률 계산하기
+aapl=aapl.dropna()
+aapl.columns=['date','closed_price']
+aapl['returns']=(aapl['closed_price']-aapl['closed_price'].shift(-1))/aapl['closed_price'].shift(-1)
 
-## 칼럼 이름 넣기
-df.columns=['date','closed_price']
+#누적 수익률 구하기
+cum_r=aapl['returns'].iloc[::-1].add(1).cumprod()
+dd=cum_r.div(cum_r.cummax()).sub(1)
 
-## 역순으로 배열하기
-aapl=df.iloc[::-1]
-
-## draw down 칼럼을 만들어 계산하기
-for i in range(0,len(aapl)-1):
-    aapl['draw_down']=(aapl['closed_price'].iloc[i]-aapl['closed_price'].iloc[:i+1].max())/aapl['closed_price'].iloc[i]
-
-## draw down 중 최댓값 찾기
-aapl['draw_down'].max()
+#누적수익률 중 최솟값 찾기
+mdd=dd.min()
